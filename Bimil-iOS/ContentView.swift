7be6a8f69760704b20cd3274fc28a2714073e68a
GyuminLee <gyumin.lee@rwth-aachen.de>
2019-12-05 20:00:02 +0100

    first commit

---
+++ Bimil-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bimil-iOS
+//
+//  Created by Gyumin on 05.12.19.
+//  Copyright © 2019 KarlsBubu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
